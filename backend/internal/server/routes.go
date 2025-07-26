1a949b9178ed7367ed86480914bca66c1cb49404
Moukhtar Youssef Moukhtar Elsayed Abdelnabi <101397758+Moukhtar-youssef@users.noreply.github.com>
2025-07-26 04:28:26 +0300

    test

--- backend/internal/server/routes.go
+++ backend/internal/server/routes.go
@@ -2,13 +2,8 @@ package server
 
 import (
 	"encoding/json"
-	"encoding/xml"
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"reflect"
-	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -61,71 +56,71 @@ func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(jsonResp)
 }
 
-func parseRequest(r *http.Request, target any) error {
-	contentType := r.Header.Get("Content-Type")
-
-	if contentType == "application/json" {
-		return json.NewDecoder(r.Body).Decode(target)
-	}
-
-	if contentType == "application/xml" || contentType == "text/xml" {
-		return xml.NewDecoder(r.Body).Decode(target)
-	}
-
-	if contentType == "application/x-www-form-urlencoded" {
-		if err := r.ParseForm(); err != nil {
-			return err
-		}
-		return formToStruct(r.Form, target)
-	}
-
-	if r.URL.Query().Get("") != "" {
-		return formToStruct(r.URL.Query(), target)
-	}
-	if r.Header.Get("") != "" {
-		return headerToStruct(r.Header, target)
-	}
-	return fmt.Errorf("unsupported content type: %s", contentType)
-}
-
-func formToStruct(form url.Values, target any) error {
-	val := reflect.ValueOf(target).Elem()
-	typ := val.Type()
-
-	for i := range val.NumField() {
-		field := typ.Field(i)
-		tag := field.Tag.Get("form")
-		if tag == "" {
-			tag = strings.ToLower(field.Name)
-		}
-
-		if values, ok := form[tag]; ok && len(values) > 0 {
-			fieldValue := val.Field(i)
-			if fieldValue.CanSet() {
-				fieldValue.SetString(values[0])
-			}
-		}
-	}
-	return nil
-}
-
-func headerToStruct(header http.Header, target any) error {
-	val := reflect.ValueOf(target).Elem()
-	typ := val.Type()
-
-	for i := range val.NumField() {
-		field := typ.Field(i)
-		tag := field.Tag.Get("header")
-		if tag == "" {
-			tag = strings.ToLower(field.Name)
-		}
-
-		if values, ok := header[tag]; ok && len(values) > 0 {
-			fieldValue := val.Field(i)
-			if fieldValue.CanSet() {
-				fieldValue.SetString(values[0])
-			}
-		}
-	}
-	return nil
-}
+// func parseRequest(r *http.Request, target any) error {
+// 	contentType := r.Header.Get("Content-Type")
+//
+// 	if contentType == "application/json" {
+// 		return json.NewDecoder(r.Body).Decode(target)
+// 	}
+//
+// 	if contentType == "application/xml" || contentType == "text/xml" {
+// 		return xml.NewDecoder(r.Body).Decode(target)
+// 	}
+//
+// 	if contentType == "application/x-www-form-urlencoded" {
+// 		if err := r.ParseForm(); err != nil {
+// 			return err
+// 		}
+// 		return formToStruct(r.Form, target)
+// 	}
+//
+// 	if r.URL.Query().Get("") != "" {
+// 		return formToStruct(r.URL.Query(), target)
+// 	}
+// 	if r.Header.Get("") != "" {
+// 		return headerToStruct(r.Header, target)
+// 	}
+// 	return fmt.Errorf("unsupported content type: %s", contentType)
+// }
+//
+// func formToStruct(form url.Values, target any) error {
+// 	val := reflect.ValueOf(target).Elem()
+// 	typ := val.Type()
+//
+// 	for i := range val.NumField() {
+// 		field := typ.Field(i)
+// 		tag := field.Tag.Get("form")
+// 		if tag == "" {
+// 			tag = strings.ToLower(field.Name)
+// 		}
+//
+// 		if values, ok := form[tag]; ok && len(values) > 0 {
+// 			fieldValue := val.Field(i)
+// 			if fieldValue.CanSet() {
+// 				fieldValue.SetString(values[0])
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
+//
+// func headerToStruct(header http.Header, target any) error {
+// 	val := reflect.ValueOf(target).Elem()
+// 	typ := val.Type()
+//
+// 	for i := range val.NumField() {
+// 		field := typ.Field(i)
+// 		tag := field.Tag.Get("header")
+// 		if tag == "" {
+// 			tag = strings.ToLower(field.Name)
+// 		}
+//
+// 		if values, ok := header[tag]; ok && len(values) > 0 {
+// 			fieldValue := val.Field(i)
+// 			if fieldValue.CanSet() {
+// 				fieldValue.SetString(values[0])
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
