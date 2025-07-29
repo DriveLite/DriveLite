e2cfa96fe83cb389d641dbc26947aa3b35958946
Moukhtar Youssef Moukhtar Elsayed Abdelnabi <101397758+Moukhtar-youssef@users.noreply.github.com>
2025-07-29 13:53:23 +0300

    Starting the development of the landing page

--- landing_page/src/app/page.tsx
+++ landing_page/src/app/page.tsx
@@ -2,15 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen place-items-end pr-8">
-      <Image
-        src="/logo.svg"
-        alt="Next.js Logo"
-        width={300}
-        height={300}
-        className="dark:invert m-0"
-        priority
-      />
-    </div>
+    <Image
+      src="/logo.svg"
+      alt="Drive lite Logo"
+      width={300}
+      height={300}
+      className=""
+      priority
+    />
   );
 }
