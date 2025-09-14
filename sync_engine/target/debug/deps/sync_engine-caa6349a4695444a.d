e46e358a7731c3fc2ff170d4ca77e90efc105277
Moukhtar-youssef <moukhtar.youssef06@gmail.com>
2025-09-14 05:33:53 +0200

    Adding the basic structure for desktop and sync_engine

---
+++ sync_engine/target/debug/deps/sync_engine-caa6349a4695444a.d
@@ -0,0 +1,5 @@
+/Users/moukhtar/projects/DriveLite/sync_engine/target/debug/deps/sync_engine-caa6349a4695444a.d: src/lib.rs
+
+/Users/moukhtar/projects/DriveLite/sync_engine/target/debug/deps/libsync_engine-caa6349a4695444a.rmeta: src/lib.rs
+
+src/lib.rs:
